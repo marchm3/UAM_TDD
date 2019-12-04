cb7e7799cebda7fc84668b0349bc36bcf0e069b8
radek <radoslaw.weychan@gmail.com>
2019-12-04 12:28:30 +0100

    ArchUnit simple test

---
+++ src/test/java/com/capgemini/tdd/archunit/CodingRulesTest.java
@@ -0,0 +1,40 @@
+package com.capgemini.tdd.archunit;
+
+import com.tngtech.archunit.base.DescribedPredicate;
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+import org.junit.Test;
+
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.fields;
+
+@AnalyzeClasses(packages = "com.capgemini.tdd")
+public class CodingRulesTest
+{
+
+    @ArchTest
+    public static final ArchRule fieldsShouldBePrivate =
+            fields().should().bePrivate().because("we agreed on this convention");
+
+    @ArchTest
+    public static final ArchRule serviceName = classes().should().haveSimpleNameEndingWith("Impl");
+
+    @Test
+    public void ruleCheckForFields()
+    {
+        JavaClasses importedClasses = new ClassFileImporter().importPackages("com.capgemini.tdd.services.game.impl");
+        fieldsShouldBePrivate.check(importedClasses);
+    }
+
+    @Test
+    public void checkServiceImplName()
+    {
+        JavaClasses importedClasses = new ClassFileImporter().importPackages("com.capgemini.tdd.core.service.impl");
+        serviceName.check(importedClasses);
+    }
+
+}
